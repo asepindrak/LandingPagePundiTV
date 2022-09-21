04ec139af05b6a6becefd7aa6ec4f83550cea4f3
unknown <you@example.com>
2022-09-21 16:28:48 +0700

    Landing Page Pundi TV

--- pages/components/get_app.tsx
+++ pages/components/get_app.tsx
@@ -9,17 +9,15 @@ export default function GetApp() {
                         Dont miss Pundi TV App on your mobile or tablets
                     </p>
                     <p className="mt-5 text-2xl font-medium text-justify font-caveat lg:text-3xl indent-20 lg:mt-10">
-                        Download the pundi application and make your channel more viewers and subscribers, don't forget to watch videos and earn money.
+                        Download the pundi application and make your channel more viewers and subscribers, dont forget to watch videos and earn money.
                     </p>
                     <div className="flex justify-center mt-5 space-x-5 md:mt-10">
                         <button className="transition ease-in-out bg-black shadow-2xl rounded-xl hover:scale-110 ">
-                            {/* <img src="/images/playStore.png" alt="" className="md:h-12 lg:h-16" /> */}
                             <div className="relative h-16 w-44">
                                 <Image src="/images/playStore.png" alt="App Pundi TV" className='w-56 mx-auto rounded-2xl' layout="fill" objectFit="cover" />
                             </div>
                         </button>
                         <button className="transition ease-in-out bg-black shadow-2xl rounded-xl hover:scale-110 ">
-                            {/* <img src="/images/appStore.png" alt="" className="md:h-12 lg:h-16" /> */}
                             <div className="relative h-16 w-44">
                                 <Image src="/images/appStore.png" alt="App Pundi TV" className='w-56 mx-auto rounded-2xl' layout="fill" objectFit="cover" />
                             </div>
@@ -28,8 +26,6 @@ export default function GetApp() {
                 </div>
             </div>
             <div className="items-center justify-center mt-10 space-y-5 md:space-y-0 lg:mt-0 md:flex md:space-x-5 lg:w-2/3">
-                {/* <img src="/images/pundi1.jpeg" alt="" className="w-64 mx-auto transition ease-in-out shadow-2xl lg:w-96 rounded-2xl hover:scale-105 md:mx-0" /> */}
-                {/* <img src="/images/pundi2.jpeg" alt="" className="w-64 mx-auto transition ease-in-out shadow-2xl lg:w-96 rounded-2xl hover:scale-105 md:mx-0" /> */}
                 <div className="relative mx-auto lg:h-500 lg:w-96 w-72 h-400 rounded-2xl md:mx-0">
                     <Image src="/images/pundi1.jpeg" alt="App Pundi TV" className='w-56 mx-auto rounded-2xl' layout="fill" objectFit="cover" />
                 </div>
